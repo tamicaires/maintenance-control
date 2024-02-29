85173fc48dbe23b7740b241f04252e3f0ff7a5bb
Tamires Caires <tamiresdec@gmail.com>
2024-02-29 17:48:55 -0300

    feat(fleet): implement handling for missing carrierId relationship

---
+++ src/modules/fleet/exceptions/CarrierIdNotFoundException.ts
@@ -0,0 +1,11 @@
+import { HttpStatus } from "@nestjs/common";
+import { AppException } from "src/exceptions/appException";
+
+export class CarrierIdNotFoundException extends AppException {
+  constructor(){
+    super({
+      message: 'Transportadora não foi encontrada, cadastre sua frota com uma transportadora existente',
+      status: HttpStatus.NOT_FOUND
+    });
+  };
+};
