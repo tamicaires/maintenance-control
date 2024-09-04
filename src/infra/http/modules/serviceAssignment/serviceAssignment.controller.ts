400d3d5e2ef9a125c6dcefa0781e7fb062008b01
tamicaires <tamiresdec@gmail.com>
2024-09-04 16:05:18 -0300

    chore: apply ESLint and Prettier fixes to service assigment controller
    - Formatted code according to ESLint and Prettier rules.
    - Improved code readability and consistency.

--- src/infra/http/modules/serviceAssignment/serviceAssignment.controller.ts
+++ src/infra/http/modules/serviceAssignment/serviceAssignment.controller.ts
@@ -1,12 +1,21 @@
-import { Body, Controller, Delete, Get, Param, Post, Put, Query } from "@nestjs/common";
-import { CreateServiceAssignmentBody } from "./dtos/CreateServiceAssignmentBody";
-import { CreateServiceAssignment } from "src/modules/serviceAssignment/useCases/createServiceAssignment/createServiceAssignment";
-import { UpdateServiceAssignmentBody } from "./dtos/updateServiceAssignmentBody";
-import { UpdateServiceAssignment } from "src/modules/serviceAssignment/useCases/updateServiceAssignment/updateServiceAssignment";
-import { DeleteServiceAssignment } from "src/modules/serviceAssignment/useCases/deleteServiceAssignment/deleteServiceAssignment";
-import { GetServiceAssignment } from "src/modules/serviceAssignment/useCases/getServiceAssignment/getServiceAssignment";
-import { ServiceAssignmentViewModel } from "./viewModels/ServiceAssignmentViewModel";
-import { GetManyServiceAssignments } from "src/modules/serviceAssignment/useCases/getManyServiceAssignments/getManyServiceAssignments";
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
+import { CreateServiceAssignmentBody } from './dtos/CreateServiceAssignmentBody';
+import { CreateServiceAssignment } from 'src/modules/serviceAssignment/useCases/createServiceAssignment/createServiceAssignment';
+import { UpdateServiceAssignmentBody } from './dtos/updateServiceAssignmentBody';
+import { UpdateServiceAssignment } from 'src/modules/serviceAssignment/useCases/updateServiceAssignment/updateServiceAssignment';
+import { DeleteServiceAssignment } from 'src/modules/serviceAssignment/useCases/deleteServiceAssignment/deleteServiceAssignment';
+import { GetServiceAssignment } from 'src/modules/serviceAssignment/useCases/getServiceAssignment/getServiceAssignment';
+import { ServiceAssignmentViewModel } from './viewModels/ServiceAssignmentViewModel';
+import { GetManyServiceAssignments } from 'src/modules/serviceAssignment/useCases/getManyServiceAssignments/getManyServiceAssignments';
 
 @Controller('service-assignments')
 export class ServiceAssignmentController {
@@ -15,65 +24,65 @@ export class ServiceAssignmentController {
     private updateServiceAssignment: UpdateServiceAssignment,
     private deleteServiceAssignment: DeleteServiceAssignment,
     private getServiceAssignment: GetServiceAssignment,
-    private getManyServiceAssignment: GetManyServiceAssignments
-  ){}
+    private getManyServiceAssignment: GetManyServiceAssignments,
+  ) {}
 
   @Post()
-  async create(@Body() body: CreateServiceAssignmentBody){
+  async create(@Body() body: CreateServiceAssignmentBody) {
     const { workOrderId, serviceId, employeeId } = body;
 
     const serviceAssignment = await this.createServiceAssignment.execute({
       workOrderId,
       serviceId,
-      employeeId
+      employeeId,
     });
 
     return serviceAssignment;
-  };
+  }
 
   @Put(':id')
   async update(
     @Param('id') serviceAssignmentId: string,
-    @Body() body: UpdateServiceAssignmentBody   
-  ){
+    @Body() body: UpdateServiceAssignmentBody,
+  ) {
     const { workOrderId, serviceId, employeeId } = body;
 
     const serviceAssignment = await this.updateServiceAssignment.execute({
       serviceAssignmentId: serviceAssignmentId,
       workOrderId,
       serviceId,
-      employeeId
+      employeeId,
     });
 
     return serviceAssignment;
-  };
+  }
 
   @Delete(':id')
-  async delete(@Param('id') serviceAssignmentId: string){
+  async delete(@Param('id') serviceAssignmentId: string) {
     await this.deleteServiceAssignment.execute({
-      serviceAssignmentId
+      serviceAssignmentId,
     });
-  };
+  }
 
   @Get(':id')
-  async getOne(@Param('id') serviceAssignmentId: string){
+  async getOne(@Param('id') serviceAssignmentId: string) {
     const serviceAssignment = await this.getServiceAssignment.execute({
-      serviceAssignmentId
+      serviceAssignmentId,
     });
 
-    return ServiceAssignmentViewModel.toHttp(serviceAssignment); 
-  };
+    return serviceAssignment;
+  }
 
   @Get()
   async getMany(
     @Query('page') page: string,
-    @Query('perPage') perPage: string
-  ){
+    @Query('perPage') perPage: string,
+  ) {
     const serviceAssignments = await this.getManyServiceAssignment.execute({
       page,
-      perPage
+      perPage,
     });
 
     return serviceAssignments.map(ServiceAssignmentViewModel.toHttp);
-  };
-};
+  }
+}
