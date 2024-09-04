ea8580a30436ad65c5859968466c8839f5e2fe4d
tamicaires <tamiresdec@gmail.com>
2024-09-04 16:04:19 -0300

    chore: apply ESLint and Prettier fixes to work order controller
    - Formatted code according to ESLint and Prettier rules.
    - Improved code readability and consistency.

--- src/infra/http/modules/workOrder/workOrder.controller.ts
+++ src/infra/http/modules/workOrder/workOrder.controller.ts
@@ -1,26 +1,27 @@
-import { 
-  Body, 
-  Controller, 
-  Delete, 
-  Get, 
-  Param, 
-  Post, 
-  Put, 
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
   Query,
-  Request 
-} from "@nestjs/common";
-import { CreateWorkOrder } from "src/modules/workOrder/useCases/createWorkOrder/createWorkOrder";
-import { CreateWorkOrderBody } from "./dtos/createWorkOrderBody";
-import { UpdateWorkOrderBody } from "./dtos/updateWorkOrderBody";
-import { UpdateWorkOrder } from "src/modules/workOrder/useCases/updateWorkOrder/updateWorkOrder";
-import { mapCreateWorkOrderData, mapUpdateWorkOrderData } from "src/utils/workOrderUtils";
-import { DeleteWorkOrder } from "src/modules/workOrder/useCases/deleteWorkOrder/deleteWorkOrder";
-import { WorkOrderViewModel } from "./viewModels/workOrderViewModel";
-import { GetManyWorkOrders } from "src/modules/workOrder/useCases/getManyWorkOrders/getManyWorkOrders";
-import { AuthenticatedRequestModel } from "../auth/models/authenticateRequestModel";
-import { GetWorkOrderServices } from "src/modules/workOrder/useCases/getWorkOrderWithServices/getWorkOrderWithServices";
-
-
+  Request,
+} from '@nestjs/common';
+import { CreateWorkOrder } from 'src/modules/workOrder/useCases/createWorkOrder/createWorkOrder';
+import { CreateWorkOrderBody } from './dtos/createWorkOrderBody';
+import { UpdateWorkOrderBody } from './dtos/updateWorkOrderBody';
+import { UpdateWorkOrder } from 'src/modules/workOrder/useCases/updateWorkOrder/updateWorkOrder';
+import {
+  mapCreateWorkOrderData,
+  mapUpdateWorkOrderData,
+} from 'src/utils/workOrderUtils';
+import { DeleteWorkOrder } from 'src/modules/workOrder/useCases/deleteWorkOrder/deleteWorkOrder';
+import { WorkOrderViewModel } from './viewModels/workOrderViewModel';
+import { GetManyWorkOrders } from 'src/modules/workOrder/useCases/getManyWorkOrders/getManyWorkOrders';
+import { AuthenticatedRequestModel } from '../auth/models/authenticateRequestModel';
+import { GetWorkOrderServices } from 'src/modules/workOrder/useCases/getWorkOrderWithServices/getWorkOrderWithServices';
 
 @Controller('work-orders')
 export class WorkOrderController {
@@ -29,72 +30,71 @@ export class WorkOrderController {
     private updateWorkOrder: UpdateWorkOrder,
     private deleteWorkOrder: DeleteWorkOrder,
     private getManyWorkOrder: GetManyWorkOrders,
-    private getWorkOrderServices: GetWorkOrderServices
-  ){}
+    private getWorkOrderServices: GetWorkOrderServices,
+  ) {}
 
   @Post()
   async create(
     @Request() request: AuthenticatedRequestModel,
-    @Body() createWorkOrderBody: CreateWorkOrderBody){
+    @Body() createWorkOrderBody: CreateWorkOrderBody,
+  ) {
     const currentUser = request.user;
-    console.log(createWorkOrderBody)
+    console.log(createWorkOrderBody);
     const workOrderData = mapCreateWorkOrderData(
       currentUser.id,
       currentUser.name,
-      createWorkOrderBody
+      createWorkOrderBody,
     );
 
-    const workOrder = await this.createWorkOrder.execute(
-      workOrderData
-    );
-    
-    return workOrder
-  };
+    const workOrder = await this.createWorkOrder.execute(workOrderData);
+
+    return workOrder;
+  }
 
   @Put(':id')
   async update(
     @Request() request: AuthenticatedRequestModel,
     @Param('id') workOrderId: string,
-    @Body() updateWorkOrderBody: UpdateWorkOrderBody
-  ){
+    @Body() updateWorkOrderBody: UpdateWorkOrderBody,
+  ) {
     const currentUser = request.user;
 
     const workOrderData = mapUpdateWorkOrderData(
       workOrderId,
       currentUser.id,
       currentUser.name,
-      updateWorkOrderBody
+      updateWorkOrderBody,
     );
-      console.log('controler', workOrderData)
+
     const workOrder = await this.updateWorkOrder.execute(workOrderData);
 
     return workOrder;
-  };
+  }
 
   @Delete(':id')
-  async delete(@Param('id') workOrderId: string){
+  async delete(@Param('id') workOrderId: string) {
     await this.deleteWorkOrder.execute({ workOrderId });
-  };
+  }
 
   @Get(':id')
-  async getOneWithServices(@Param('id') workOrderId: string){
+  async getOneWithServices(@Param('id') workOrderId: string) {
     const workOrderServices = await this.getWorkOrderServices.execute({
-      workOrderId
+      workOrderId,
     });
-    
+
     return workOrderServices;
-  };
+  }
 
   @Get()
   async getMany(
     @Query('page') page: string,
-    @Query('perPage') perPage: string
-  ){
+    @Query('perPage') perPage: string,
+  ) {
     const workOrders = await this.getManyWorkOrder.execute({
       page,
-      perPage
+      perPage,
     });
-    
+
     return workOrders.map(WorkOrderViewModel.toHttp);
-  };
-};
+  }
+}
