9b8678c90b5d9139f855d5ddf13e53c461d64a96
tamicaires <tamiresdec@gmail.com>
2025-01-07 18:24:57 -0300

    feat: implement delete checklist item

--- src/presenters/checklist/checklist-item/checklist-item.controller.ts
+++ src/presenters/checklist/checklist-item/checklist-item.controller.ts
@@ -1,16 +1,18 @@
-import { Body, Controller, Delete, Param, Post } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, Post } from "@nestjs/common";
 import { CreateChecklistItem } from "src/application/checklist/checklist-item/use-cases/create-checklist-item";
 import { Cookies } from "src/infra/http/auth/decorators/cookies.decorator";
 import { CreateChecklistItemDTO } from "./dtos/create-checklist-item";
 import { CompanyInstance } from "src/core/company/company-instance";
 import { CookiesEnum } from "src/core/enum/cookies";
 import { DeleteChecklistItem } from "src/application/checklist/checklist-item/use-cases/delete-checklist-item";
+import { ListChecklistItemByChecklistId } from "src/application/checklist/checklist-item/use-cases/list-checklist-item";
 
 @Controller('checklist-item')
 export class ChecklistItemController {
   constructor(
     private readonly _createChecklistItem: CreateChecklistItem,
-    private readonly _deleteChecklistItem: DeleteChecklistItem
+    private readonly _deleteChecklistItem: DeleteChecklistItem,
+    private readonly _listChecklistItem: ListChecklistItemByChecklistId
   ) { }
 
   @Post()
@@ -21,6 +23,16 @@ export class ChecklistItemController {
     const companyInstance = CompanyInstance.create(companyId);
 
     return this._createChecklistItem.execute(companyInstance, body);
+  }
+
+  @Get('checklist/:id')
+  async list(
+    @Cookies(CookiesEnum.CompanyId) companyId: string,
+    @Param('id') checklistId: string
+  ) {
+    const companyInstance = CompanyInstance.create(companyId);
+
+    return this._listChecklistItem.execute(companyInstance, checklistId);
   }
 
   @Delete(':id')
--- src/presenters/checklist/checklist-item/checklist-item.module.ts
+++ src/presenters/checklist/checklist-item/checklist-item.module.ts
@@ -3,12 +3,14 @@ import { ChecklistItemController } from "./checklist-item.controller";
 import { DatabaseModule } from "src/infra/database/database.module";
 import { CreateChecklistItem } from "src/application/checklist/checklist-item/use-cases/create-checklist-item";
 import { DeleteChecklistItem } from "src/application/checklist/checklist-item/use-cases/delete-checklist-item";
+import { ListChecklistItemByChecklistId } from "src/application/checklist/checklist-item/use-cases/list-checklist-item";
 
 @Module({
   controllers: [ChecklistItemController],
   imports: [DatabaseModule],
   providers: [
     CreateChecklistItem,
+    ListChecklistItemByChecklistId,
     DeleteChecklistItem,
   ]
 })
